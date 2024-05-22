e2542619253b473ca46c07477a1ab1e2aa55ffee
Louis LeGars <49105384+Antarcy@users.noreply.github.com>
2024-05-22 12:30:42 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,10 @@ author 'atiysu'
 game 'gta5'
 description 'aty_icehud'
 
-shared_script 'config.lua'
+shared_script {
+    '@es_extended/imports.lua',
+    'config.lua'
+}
 client_script 'client/client.lua'
 server_script 'server/server.lua'
 ui_page 'ui/index.html'
